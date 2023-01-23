d2b23db46a18d98dd823bd5adad9a95bdfb6e6d9
shanep42 <107292088+shanep42@users.noreply.github.com>
2023-01-22 23:07:34 -0600

    Adds user routes/controllers, jwt, authorization

---
+++ backend/models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter a name'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email'],
+        unique: true,
+    },
+    password: {
+        type: String,
+        requied: [true, 'Please add a password']
+    }
+},
+{
+    timestamp: true
+}
+)
+
+module.exports = mongoose.model('User', userSchema)
